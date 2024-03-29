20c852627a1ce818ca30da7a69d9141508d496dd
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-03-29 14:56:51 +0900

    アニメーション時のjsファイル追加

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(function () {
+  $(".hamburger").on("click", function () {
+    hamburger()
+  })
+  $(".header-list a").on("click", function () {
+    hamburger()
+  })
+
+  $(".inview-slide-left").on("inview", function (
+    event,
+    isInView,
+    visiblePartX,
+    visiblePartY
+  ) {
+    if (isInView) {
+      $(this).stop().addClass("slide-left")
+    }
+  })
+
+  $(".inview-slide-right").on("inview", function (
+    event,
+    isInView,
+    visiblePartX,
+    visiblePartY
+  ) {
+    if (isInView) {
+      $(this).stop().addClass("slide-right")
+    }
+  })
+
+  $(".inview-balloon").on("inview", function (
+    event,
+    isInView,
+    visiblePartX,
+    visiblePartY
+  ) {
+    if (isInView) {
+      $(this).stop().addClass("balloon")
+    }
+  })
+})
+
+function hamburger() {
+  $(".hamburger").toggleClass("active")
+
+  if ($(".hamburger").hasClass("active")) {
+    $(".header-list").addClass("active")
+  } else {
+    $(".header-list").removeClass("active")
+  }
+}
